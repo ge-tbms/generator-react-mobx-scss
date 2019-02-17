d31b512d3dc13ea48aa9b062034fe8092d21c1e5
罗嗣 <fangbo.fb@alibaba-inc.com>
2019-02-17 21:12:28 +0800

    init

---
+++ generators/app/templates/README.md
@@ -0,0 +1,15 @@
+# <%= answers.name%> with React + MobX + SCSS
+
+This repository provides a reference implementation of the [TodoMVC](http://todomvc.com) application written using [MobX](https://github.com/mobxjs/mobx), [React](https://facebook.github.io/react) JSX and ES6.
+
+## Running the example
+
+```
+npm install
+npm start
+open http://localhost:9000
+```
+
+The example requires node 4.0 or higher
+
+![TodoMVC](devtools.gif)
---
+++ generators/app/templates/webpack.config.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HappyPack = require('happypack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+
+
+const NODE_MODULES_PATH = path.resolve(__dirname, 'node_modules');
+
+const config = {
+  mode: 'none',
+  optimization: {
+    minimize: false,
+    splitChunks: {
+      chunks: 'async',
+      name: true,
+      cacheGroups: {
+        vendor: { test: /[\\/]node_modules[\\/]/, name: 'vendor', chunks: 'all', priority: 2, minChunks: 2, }
+      }
+    }
+  },
+  entry: {
+    bundle: './src/index.js',
+    vendor: ['react', 'react-dom', 'mobx-react', 'mobx', 'director/build/director', 'prop-types']
+  },
+  output: { path: path.join(__dirname, 'static'), filename: '[name].js', publicPath: '/static/' },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+    new HappyPack({ id: 'jsx', threads: 4, loaders: ['babel-loader']}),
+  ],
+  resolve: { extensions: ['.js', '.jsx'] },
+  module: {
+    rules: [
+      { test: /\.jsx|js?$/, use: 'happypack/loader?id=jsx', exclude: NODE_MODULES_PATH },
+      { test: /\.(sa|sc|c)ss$/, use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'], exclude: NODE_MODULES_PATH }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, './static'),
+    compress: true,
+    port: 9000
+  }
+};
+
+
+console.warn('current environment is', process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  config.mode = 'development';
+} else if (process.env.NODE_ENV === 'analyzer') {
+  config.plugins.push(
+    new BundleAnalyzerPlugin()
+  );
+} else if (process.env.NODE_ENV  === 'production') {
+  config.plugins.push(
+    new OptimizeCSSAssetsPlugin()
+  );
+  config.mode = 'production';
+  config.optimization.minimize = true;
+}
+
+module.exports = config;
